eecbaaf67fb4cb1fa653af939b0b87055b5341e3
yuaori <yuaori@bytedance.com>
2022-08-08 14:20:04 +0800

    fix: transform rem to px

--- postcss.config.js
+++ postcss.config.js
@@ -2,5 +2,14 @@ module.exports = {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
+    'postcss-rem-to-pixel': {
+      rootValue: 16,
+      unitPrecision: 5,
+      propList: ['*'],
+      selectorBlackList: [],
+      replace: true,
+      mediaQuery: false,
+      minRemValue: 0,
+    },
   },
-}
+};
